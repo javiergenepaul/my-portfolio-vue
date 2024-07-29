47699f3ab025f32c73f7291d4e9c63e485e4fe66
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-29 16:32:23 +0800

    adding header home view placeholder

--- src/views/Home/components/HomeHeader.vue
+++ src/views/Home/components/HomeHeader.vue
@@ -1,22 +1,68 @@
 <script setup lang="ts">
-import { Avatar, AvatarFallback, AvatarImage, Badge, Card, CardContent, CardHeader, Separator } from '@/components/ui';
+import { Avatar, AvatarFallback, AvatarImage, Badge, Card, CardContent, CardFooter, CardHeader, Separator } from '@/components/ui';
 import { translate } from '@/lib';
+import ContactInfo, { ContactInfoInterface } from './ContactInfo.vue';
+import SocialLinks, { SocialLinksInterface } from './SocialLinks.vue';
+
+const CONTACT_INFOS: ContactInfoInterface[] = [
+    {
+        title: "EMAIL",
+        value: "javiergenepaul@gmail.com"
+    },
+    {
+        title: "PHONE",
+        value: "+639 762 912 231"
+    },
+    {
+        title: "BIRTHDATE",
+        value: "July 20, 1998"
+    },
+    // {
+    //     title: "LOCATION",
+    //     value: "San Isidro Labrador, Quiot Greenbelt Drive St. Cebu City, Cebu Philippines"
+    // }
+]
+
+const SOCIAL_LINKS: SocialLinksInterface[] = [
+    {
+        title: "Github",
+        url: "https://github.com/javiergenepaul"
+    },
+    {
+        title: "LinkedIn",
+        url: "https://www.linkedin.com/in/gene-paul-mar-javier-500b93245/"
+    },
+    {
+        title: "Upwork",
+        url: "https://www.upwork.com/freelancers/~0146c967e12a6bff87"
+    }
+]
+
 </script>
 
 <template>
-    <Card>
+    <Card class="h-full">
         <CardHeader class="items-center">
             <Avatar size="lg">
                 <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <h1 class="text-2xl">{{ translate("home.name") }}</h1>
+            <h1 class="text-2xl font-bold">{{ translate("home.name") }}</h1>
             <Badge>{{ translate("home.occupation") }}</Badge>
             <Separator class="mt-4" />
         </CardHeader>
         <CardContent>
-            contetn
+            <div class="flex flex-col gap-4">
+                <ContactInfo v-for="(contact, index) in CONTACT_INFOS" :key="index" :title="contact.title"
+                    :value="contact.value" />
+            </div>
         </CardContent>
+        <CardFooter class="mt-28 justify-center">
+            <div class="flex items-center justify-center gap-4">
+                <SocialLinks v-for="(social, index) in SOCIAL_LINKS" :key="index" :title="social.title"
+                    :url="social.url" />
+            </div>
+        </CardFooter>
     </Card>
 
 </template>
