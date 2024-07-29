73657402fcac969832d136dcde53d6ae59190e92
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-29 15:39:45 +0800

    feat: add files for home view

---
+++ src/views/Home/components/HomeBody.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { translate } from '@/lib';
+</script>
+
+<template>
+    <body>
+        <nav>
+            <ul class="flex gap-2">
+                <li class="underline">{{ translate("nav.about") }}</li>
+                <li class="underline">{{ translate("nav.project") }}</li>
+                <li class="underline">{{ translate("nav.skills") }}</li>
+                <li class="underline">{{ translate("nav.contact") }}</li>
+            </ul>
+        </nav>
+    </body>
+</template>
---
+++ src/views/Home/components/HomeHeader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { translate } from '@/lib';
+</script>
+
+<template>
+    <header class="basis-1/3">
+        <span>{{ translate("helloWorld") }}</span>
+    </header>
+</template>
