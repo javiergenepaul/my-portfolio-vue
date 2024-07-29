1c6195dc739c563093b8e7128184c4a9c23ec9d1
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-29 16:07:58 +0800

    feat: add home header badge name and avatar

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export * from "./button";
+export * from "./card";
+export * from "./avatar";
+export * from "./separator";
+export * from "./badge";
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -4,9 +4,11 @@ import { HomeBody, HomeView } from './components';
 </script>
 
 <template>
-  <div class="flex py-12 px-8 gap-4">
-    <HomeView />
-    <body class="basis-2/3">
+  <div class="flex py-12 px-8 gap-4 h-screen">
+    <header class="basis-1/4">
+      <HomeView />
+    </header>
+    <body class="basis-3/4">
       <HomeBody />
       <RouterView />
     </body>
