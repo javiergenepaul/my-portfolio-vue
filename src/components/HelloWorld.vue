02fc09e0adf063311cf3fb94b60a9464a98eeea4
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-29 15:02:21 +0800

    feat: add translate utility

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { Button } from '@/components/ui/button'
+import { translate } from '@/lib/translate';
 
 defineProps<{ msg: string }>()
 
@@ -15,23 +16,18 @@ const count = ref(0)
     <Button @click="count++">count is {{ count }}</Button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>{{ translate("helloWorld") }}</code>
     </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
