c00eeef63db96dea289b1719ce4b540a3c9d39d4
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-29 16:07:58 +0800

    feat: add home header badge name and avatar

--- src/views/Home/components/HomeBody.vue
+++ src/views/Home/components/HomeBody.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
+import { Card, CardHeader } from '@/components/ui';
 import { translate } from '@/lib';
 </script>
 
 <template>
-    <body>
-        <nav>
-            <ul class="flex gap-2">
-                <li class="underline">{{ translate("nav.about") }}</li>
-                <li class="underline">{{ translate("nav.project") }}</li>
-                <li class="underline">{{ translate("nav.skills") }}</li>
-                <li class="underline">{{ translate("nav.contact") }}</li>
-            </ul>
-        </nav>
-    </body>
+    <Card>
+        <CardHeader>
+            <nav class="flex justify-end">
+                <ul class="flex gap-2">
+                    <li class="underline">{{ translate("nav.about") }}</li>
+                    <li class="underline">{{ translate("nav.project") }}</li>
+                    <li class="underline">{{ translate("nav.skills") }}</li>
+                    <li class="underline">{{ translate("nav.contact") }}</li>
+                </ul>
+            </nav>
+        </CardHeader>
+    </Card>
 </template>
--- src/views/Home/components/HomeHeader.vue
+++ src/views/Home/components/HomeHeader.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage, Badge, Card, CardContent, CardHeader, Separator } from '@/components/ui';
 import { translate } from '@/lib';
 </script>
 
 <template>
-    <header class="basis-1/3">
-        <span>{{ translate("helloWorld") }}</span>
-    </header>
+    <Card>
+        <CardHeader class="items-center">
+            <Avatar size="lg">
+                <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1 class="text-2xl">{{ translate("home.name") }}</h1>
+            <Badge>{{ translate("home.occupation") }}</Badge>
+            <Separator class="mt-4" />
+        </CardHeader>
+        <CardContent>
+            contetn
+        </CardContent>
+    </Card>
+
 </template>
