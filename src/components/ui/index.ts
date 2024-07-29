07f8191f03c95f6c459dd5df0801a13f38379c34
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-29 16:32:23 +0800

    adding header home view placeholder

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export * from "./card";
 export * from "./avatar";
 export * from "./separator";
 export * from "./badge";
+export * from "./tooltip";
---
+++ src/views/Home/components/ContactInfo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from '@/components';
+
+export interface ContactInfoInterface {
+    title: string;
+    value: string;
+}
+
+const props = defineProps<ContactInfoInterface>();
+</script>
+
+<template>
+    <div class="flex items-center gap-4">
+        <Avatar>
+            <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div class="flex flex-col">
+            <h4>{{ props.title }}</h4>
+            <h3>{{ props.value }}</h3>
+        </div>
+    </div>
+</template>
