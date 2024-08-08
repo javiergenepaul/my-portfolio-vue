e8f3eef41a0048962179a3e11f6c8d0ad19028d4
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-08-08 23:18:47 +0800

    fix layout for cards

--- src/views/Home/sections/ProjectSection.vue
+++ src/views/Home/sections/ProjectSection.vue
@@ -3,7 +3,7 @@ import SectionContainer from './SectionContainer.vue';
 </script>
 
 <template>
-    <SectionContainer title="Project">
+    <SectionContainer>
         Project Section
     </SectionContainer>
 </template>
